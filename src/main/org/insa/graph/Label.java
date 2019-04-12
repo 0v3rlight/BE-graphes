498d9ad6c661e0b495bc759295a6c33333740886
M. Vendeville Benjamin <vendevil@insa-08127>
2019-04-12 18:11:59 +0200

    remove et label

---
+++ src/main/org/insa/graph/Label.java
@@ -0,0 +1,16 @@
+package org.insa.graph;
+
+public class Label {
+	
+	public Node sommetCourant;
+	public Boolean marque;
+	public int cout;
+	public Node pere;
+	
+	public int getCost()
+	{
+		return 0;
+	}
+	
+
+}
