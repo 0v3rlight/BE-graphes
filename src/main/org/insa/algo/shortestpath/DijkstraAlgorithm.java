47471ee834d4e1f8dbdb46f25af16e7a196cd131
ben <ben@10.32.41.6>
2019-05-07 16:26:54 +0200

    label star et A* fini mais erreur

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -15,45 +15,69 @@ import org.insa.graph.Point;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm
 {
+	private BinaryHeap<Label> heap;
+	private Label[] labels ;
+	
+	private ShortestPathData data;
+	private ShortestPathSolution solution;
+
+	private Graph graph;
+	private List<Node> nodes;
+	private int nbNodes;
+	
+	private int originId ;
+	private int destId;
 
 	public DijkstraAlgorithm(ShortestPathData data)
 	{
 		super(data);
 	}
 
+	private void creerLabels()
+	{
+		heap = new BinaryHeap<Label>();
+		labels = new Label[nbNodes];
+	}
+	
 	@Override
 	protected ShortestPathSolution doRun()
 	{
-		ShortestPathData data = getInputData();
-		ShortestPathSolution solution = null;
+		data = getInputData();
+		solution = null;
         notifyOriginProcessed(data.getOrigin());
 
-		Graph graph = data.getGraph();
-		List<Node> nodes = graph.getNodes();
-		int nbNodes = graph.size();
+		graph = data.getGraph();
+		nodes = graph.getNodes();
+		nbNodes = graph.size();
 
-		BinaryHeap<Label> heap = new BinaryHeap<Label>();
-		Label[] labels = new Label[nbNodes];
-		int originId = data.getOrigin().getId();
-		int destId = data.getDestination().getId();
+		
+		originId = data.getOrigin().getId();
+		destId = data.getDestination().getId();
+		
 		System.out.println(originId);
 		System.out.println(destId);
+
+		creerLabels();
+		
 		for (int i = 0; i < nbNodes; i++)
 		{
 			labels[i] = new Label();
 			labels[i].sommetCourant = nodes.get(i);
 			labels[i].marque = false;
-			labels[i].cout = nodes.get(i).getId() == originId ? 0 : Double.POSITIVE_INFINITY;
 			labels[i].pere = null;
+			labels[i].cout = nodes.get(i).getId() == originId ? 0 : Double.POSITIVE_INFINITY;
 			heap.insert(labels[i]);
 		}
+		
 		Arc[] prevArcs = new Arc[nbNodes];
 		boolean finish = false;
+		
 		while (!heap.isEmpty() && !finish)
 		{
 			Label minLabel = heap.deleteMin();
 			minLabel.marque = true;
 			//System.out.println(nodeString(minLabel.sommetCourant));
+			
 			for (Arc a : minLabel.sommetCourant.getSuccessors())
 			{
 				if(minLabel.sommetCourant.getId() == destId)
@@ -61,20 +85,25 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm
 					finish = true;
 					break;
 				}
+				
 				Label label = labels[a.getDestination().getId()];
+				
 				if (label.marque)
 				{
 					continue;
 				}
-				double oldCost = label.cout;
-				double newCost = minLabel.cout + data.getCost(a);
+				
+				double oldCost = label.getTotalCost();
+				double newCost = minLabel.getTotalCost() + data.getCost(a);
+				
 				if (newCost < oldCost)
 				{
 					prevArcs[a.getDestination().getId()] = a;
 					heap.remove(label);
-					label.cout = newCost;
+					label.cout = minLabel.cout + data.getCost(a);
 					label.pere = minLabel.sommetCourant;
 					heap.insert(label);
+					
                     if(Double.isFinite(newCost) && Double.isInfinite(oldCost))
                     {
                     	notifyNodeReached(a.getDestination());
@@ -82,8 +111,10 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm
 				}
 			}
 		}
+		
 		ArrayList<Arc> arcs = new ArrayList<Arc>();
 		Node n = nodes.get(destId);
+		
 		while(n != null)
 		{
 			Arc a = prevArcs[n.getId()];
@@ -93,11 +124,14 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm
 			}
 			n = labels[n.getId()].pere;
 		}
+		
 		Collections.reverse(arcs);
+		
 		if(arcs.isEmpty())
 		{
 			solution = new ShortestPathSolution(data, Status.INFEASIBLE);
 		}
+		
 		else
 		{
 	        solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
